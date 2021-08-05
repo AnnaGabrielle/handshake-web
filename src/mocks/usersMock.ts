9bacfd41883688b88b77a681c8fd81b4a1c025cd
Lucas Murakami <lkenjim98@gmail.com>
2021-08-05 15:37:28 -0300

    create users list mock

---
+++ src/mocks/usersMock.ts
@@ -0,0 +1,38 @@
+export default [
+  {
+    firstName: 'Lucas',
+    lastName: 'Murakami',
+  },
+  {
+    firstName: 'Anna',
+    lastName: 'Lamellas',
+  },
+  {
+    firstName: 'Lucas',
+    lastName: 'Ricarte',
+  },
+  {
+    firstName: 'Livia',
+    lastName: 'Lino',
+  },
+  {
+    firstName: 'Aline',
+    lastName: 'Loureiro',
+  },
+  {
+    firstName: 'Isabel',
+    lastName: 'Firmo',
+  },
+  {
+    firstName: 'Thiago',
+    lastName: 'Simões',
+  },
+  {
+    firstName: 'Grabiel',
+    lastName: 'Félix',
+  },
+  {
+    firstName: 'Paula',
+    lastName: 'Brito',
+  },
+];
