da4b05aa12dedc75b0beb842f1af9394dd91635c
Lucas Murakami <lkenjim98@gmail.com>
2021-08-06 11:30:07 -0300

    fix meUser

--- src/mocks/me-mock.ts
+++ src/mocks/me-mock.ts
@@ -4,6 +4,7 @@ const meUser: User = {
   id: 4,
   firstName: 'Lucas',
   lastName: 'Ricarte',
+  bio: '',
   currentJob: 'Handshake office',
   photo: 'https://image.flaticon.com/icons/png/512/194/194279.png',
   interests: ['produto'],
